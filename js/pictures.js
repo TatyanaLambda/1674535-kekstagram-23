8e075d2fa93545abe0cd24a800117b7d330c62cd
Komarova Tatyana <LambdaKomar@yandex.ru>
2021-07-15 18:12:18 +0300

    Добавила открытие картинки по клику

--- js/pictures.js
+++ js/pictures.js
@@ -8,23 +8,15 @@ const pictureTemplate = document.querySelector('#picture')
 const photoDescriptions = createPhotoDescriptions();
 const photoDescriptionFragment = document.createDocumentFragment();
 
-photoDescriptions.forEach(({url, likes , comments}) => {
+photoDescriptions.forEach((photoDescription) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__img').src = photoDescription.url;
+  pictureElement.querySelector('.picture__comments').textContent = photoDescription.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = photoDescription.likes;
   photoDescriptionFragment.appendChild(pictureElement);
+  pictureElement.addEventListener('click', () => {
+    showBigPicture(photoDescription);
+  });
 });
 
 pictures.appendChild(photoDescriptionFragment);
-
-const bigPictureShower = function (evt) {
-  if (evt.target.classList.contains('picture')) {
-    evt.preventDefault();
-    const targetImage = evt.target.querySelector('.picture__img');
-    showBigPicture(targetImage);
-  }
-  showBigPicture(photoDescriptions[0]);
-};
-
-pictures.addEventListener('click', bigPictureShower);
